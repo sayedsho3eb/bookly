4b73608d3cbba53956529db1f3a7ab95f2859469
saydshoieb <103608170+sayedsho3eb@users.noreply.github.com>
2025-08-26 19:56:41 +0300

    change theme to dark and create text to add splash view body

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -9,7 +9,10 @@ class SplashViewBody extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [Image.asset(AssetsData.logo)],
+      children: [
+        Image.asset(AssetsData.logo),
+        Text(textAlign: TextAlign.center, 'Free read books'),
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,8 @@ class BooklyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      theme: ThemeData().copyWith(scaffoldBackgroundColor: kPrimaryColor),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor),
       home: SplashView(),
     );
   }
