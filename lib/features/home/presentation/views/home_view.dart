3cf217bace611d98271ff261954878fb3e6a3f10
saydshoieb <103608170+sayedsho3eb@users.noreply.github.com>
2025-08-28 11:47:48 +0300

    create custom app bar home view finish

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:booklyapp/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -5,6 +6,6 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(body: Column(children: [CustomAppBar()]));
   }
 }
---
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -0,0 +1,24 @@
+import 'package:booklyapp/core/util/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 44),
+      child: Row(
+        children: [
+          Image.asset(AssetsData.logo, width: 75),
+          Spacer(),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(weight: 19, FontAwesomeIcons.magnifyingGlass),
+          ),
+        ],
+      ),
+    );
+  }
+}
