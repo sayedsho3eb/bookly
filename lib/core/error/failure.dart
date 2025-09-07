e008417464a29ca8368d4a5bba34c2a4728ef7f8
saydshoieb <103608170+sayedsho3eb@users.noreply.github.com>
2025-09-07 10:20:22 +0300

    create repo and error message

---
+++ lib/core/error/failure.dart
@@ -0,0 +1,9 @@
+abstract class Failure {
+  final String errorMessage;
+
+  Failure(this.errorMessage);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.errorMessage);
+}
---
+++ lib/features/home/data/repo/home_repo.dart
@@ -0,0 +1,8 @@
+import 'package:booklyapp/core/error/failure.dart';
+import 'package:booklyapp/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure, List<BookModel>>> fetchFuteardBook();
+  fetchBeastSellerBook();
+}
